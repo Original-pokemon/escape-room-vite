631292bc299a467869f20230680ab7fd147273aa
Original-pokemon <eremkin-02@mail.ru>
2023-09-22 17:32:06 +0300

    refactor: use enum

---
+++ src/consts/filter.ts
@@ -0,0 +1,15 @@
+enum Theme {
+  Adventure = "adventure",
+  Horror = "horror",
+  Mystery = "mystic",
+  Detective = "detective",
+  SciFi = "sciFi",
+  AllQuests = "all",
+}
+
+enum DifficultyLevel {
+  Easy = "easy",
+  Middle = "middle",
+  Hard = "hard",
+  Any = "any",
+}
