30b3b5fe11a0acdbc81effecd9deadf061ccae19
Original-pokemon <eremkin-02@mail.ru>
2023-09-22 15:33:49 +0300

    feat: components

---
+++ src/components/app/app.tsx
@@ -0,0 +1,63 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { AppRoute } from '../../consts/app-route';
+import { authorizationStatus } from '../../consts/authorization-status';
+import Booking from '../../pages/booking';
+import Contacts from '../../pages/contacts';
+import Login from '../../pages/login';
+import Main from '../../pages/main';
+import MyQuests from '../../pages/my-quests';
+import Quest from '../../pages/quest';
+import PrivateRoute from '../private-route/private-route';
+
+export default function App(): React.JSX.Element {
+  // const authorizationStatus = useAppSelector((state) => state.authorizationStatus);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Main} >
+          <Route
+            index
+            element={<Main />}
+          />
+          <Route
+            path={AppRoute.Contacts}
+            element={<Contacts />}
+          />
+          <Route
+            path={AppRoute.MyQuests}
+            element={
+              <PrivateRoute authorizationStatus={authorizationStatus}>
+                <MyQuests />
+              </PrivateRoute>
+            }
+          />
+
+          <Route
+            path={AppRoute.Quest}
+            element={<Quest />}
+          />
+
+          <Route
+            path={AppRoute.Booking}
+            element={
+              <PrivateRoute authorizationStatus={authorizationStatus}>
+                <Booking />
+              </PrivateRoute>
+            }
+          />
+
+          <Route
+            path={AppRoute.Login}
+            element={<Login />}
+          />
+
+          {/* <Route path='*' element={<NotFoundPage />}></Route> */}
+        </Route>
+      </Routes >
+    </BrowserRouter>
+  );
+}
+
+
